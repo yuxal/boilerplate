75e91a4a6cb0aacd4cb134f41b69623276ec239d
yuval <yuvalseladev@gmail.com>
2018-03-11 17:36:14 +0200

    Add bitmap scaling util

--- app/src/main/java/boulderstudios/com/boilerplate/utils/GraphicUtils.java
+++ app/src/main/java/boulderstudios/com/boilerplate/utils/GraphicUtils.java
@@ -2,6 +2,7 @@ package boulderstudios.com.boilerplate.utils;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.util.DisplayMetrics;
 /**
  * Created by yuval on 2/24/18.
@@ -19,5 +20,26 @@ public class GraphicUtils {
         Resources resources = context.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
         return Math.round(px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public static Bitmap scaleBitmap(Bitmap bitmap, int maxWidth,int maxHeight) {
+        int currentHeight = bitmap.getHeight();
+        int currentWidth = bitmap.getWidth();
+        int newHeight = currentHeight;
+        int newWidth = currentWidth;
+
+        if (currentHeight > maxHeight) {
+            newHeight = maxHeight;
+            newWidth = currentWidth * newHeight / currentHeight;
+            currentWidth = newWidth;
+        }
+        if (newWidth > maxWidth) {
+            newWidth = maxWidth;
+            newHeight = newHeight * newWidth / currentWidth;
+        }
+        if (newWidth == bitmap.getWidth() && newHeight == bitmap.getHeight()) {
+            return bitmap;
+        }
+        return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false);
     }
 }
