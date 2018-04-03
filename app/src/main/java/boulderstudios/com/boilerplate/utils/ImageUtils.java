346ffc06cdc689350b1976c273ce2b333d080bf1
yuval <yuvalseladev@gmail.com>
2018-04-03 12:48:57 +0300

    add android commons utils

---
+++ app/src/main/java/boulderstudios/com/boilerplate/utils/ImageUtils.java
@@ -0,0 +1,158 @@
+package boulderstudios.com.boilerplate.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+/**
+ * ImageUtils
+ * <ul>
+ * convert between Bitmap, byte array, Drawable
+ * <li>{@link #bitmapToByte(Bitmap)}</li>
+ * <li>{@link #bitmapToDrawable(Bitmap)}</li>
+ * <li>{@link #byteToBitmap(byte[])}</li>
+ * <li>{@link #byteToDrawable(byte[])}</li>
+ * <li>{@link #drawableToBitmap(Drawable)}</li>
+ * <li>{@link #drawableToByte(Drawable)}</li>
+ * </ul>
+ * <ul>
+ * scale image
+ * <li>{@link #scaleImageTo(Bitmap, int, int)}</li>
+ * <li>{@link #scaleImage(Bitmap, float, float)}</li>
+ * </ul>
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2012-6-27
+ */
+public class ImageUtils {
+
+    private ImageUtils() {
+        throw new AssertionError();
+    }
+
+    /**
+     * convert Bitmap to byte array
+     * 
+     * @param b
+     * @return
+     */
+    public static byte[] bitmapToByte(Bitmap b) {
+        if (b == null) {
+            return null;
+        }
+
+        ByteArrayOutputStream o = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.PNG, 100, o);
+        return o.toByteArray();
+    }
+
+    /**
+     * convert byte array to Bitmap
+     * 
+     * @param b
+     * @return
+     */
+    public static Bitmap byteToBitmap(byte[] b) {
+        return (b == null || b.length == 0) ? null : BitmapFactory.decodeByteArray(b, 0, b.length);
+    }
+
+    /**
+     * convert Drawable to Bitmap
+     * 
+     * @param d
+     * @return
+     */
+    public static Bitmap drawableToBitmap(Drawable d) {
+        return d == null ? null : ((BitmapDrawable)d).getBitmap();
+    }
+
+    /**
+     * convert Bitmap to Drawable
+     * 
+     * @param b
+     * @return
+     */
+    public static Drawable bitmapToDrawable(Bitmap b) {
+        return b == null ? null : new BitmapDrawable(b);
+    }
+
+    /**
+     * convert Drawable to byte array
+     * 
+     * @param d
+     * @return
+     */
+    public static byte[] drawableToByte(Drawable d) {
+        return bitmapToByte(drawableToBitmap(d));
+    }
+
+    /**
+     * convert byte array to Drawable
+     * 
+     * @param b
+     * @return
+     */
+    public static Drawable byteToDrawable(byte[] b) {
+        return bitmapToDrawable(byteToBitmap(b));
+    }
+
+    /**
+     * scale image
+     * 
+     * @param org
+     * @param newWidth
+     * @param newHeight
+     * @return
+     */
+    public static Bitmap scaleImageTo(Bitmap org, int newWidth, int newHeight) {
+        return scaleImage(org, (float) newWidth / org.getWidth(), (float) newHeight / org.getHeight());
+    }
+
+    /**
+     * scale image
+     * 
+     * @param org
+     * @param scaleWidth sacle of width
+     * @param scaleHeight scale of height
+     * @return
+     */
+    public static Bitmap scaleImage(Bitmap org, float scaleWidth, float scaleHeight) {
+        if (org == null) {
+            return null;
+        }
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        return Bitmap.createBitmap(org, 0, 0, org.getWidth(), org.getHeight(), matrix, true);
+    }
+    public static Bitmap scaleBitmapUp(Bitmap bitmap, int maxWidth,int maxHeight) {
+        int currentHeight = bitmap.getHeight();
+        int currentWidth = bitmap.getWidth();
+        int newHeight = currentHeight;
+        int newWidth = currentWidth;
+
+        if (currentHeight > maxHeight) {
+            newHeight = maxHeight;
+            newWidth = currentWidth * newHeight / currentHeight;
+            currentWidth = newWidth;
+        }
+        if (newWidth > maxWidth) {
+            newWidth = maxWidth;
+            newHeight = newHeight * newWidth / currentWidth;
+        }
+        if (newWidth == bitmap.getWidth() && newHeight == bitmap.getHeight()) {
+            return bitmap;
+        }
+        return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false);
+    }
+
+}
--- app/src/main/java/boulderstudios/com/boilerplate/utils/NetworkUtils.java
+++ app/src/main/java/boulderstudios/com/boilerplate/utils/NetworkUtils.java
@@ -3,31 +3,116 @@ package boulderstudios.com.boilerplate.utils;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 
 /**
- * Created by yuval on 3/13/18.
+ * NetWork Utils
+ * <ul>
+ * <strong>Attentions</strong>
+ * <li>You should add <strong>android.permission.ACCESS_NETWORK_STATE</strong> in manifest, to get network status.</li>
+ * </ul>
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2014-11-03
  */
+public class NetWorkUtils {
 
-public class NetworkUtils {
+    public static final String NETWORK_TYPE_WIFI       = "wifi";
+    public static final String NETWORK_TYPE_3G         = "eg";
+    public static final String NETWORK_TYPE_2G         = "2g";
+    public static final String NETWORK_TYPE_WAP        = "wap";
+    public static final String NETWORK_TYPE_UNKNOWN    = "unknown";
+    public static final String NETWORK_TYPE_DISCONNECT = "disconnect";
 
-    public static boolean isOnline(Context context) {
-        NetworkInfo activeNetworkInfo = getActiveNetworkInfo(context);
-        if (activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting()) {
-            return true;
-        }
-        return false;
+    /**
+     * Get network type
+     * 
+     * @param context
+     * @return
+     */
+    public static int getNetworkType(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager == null ? null : connectivityManager.getActiveNetworkInfo();
+        return networkInfo == null ? -1 : networkInfo.getType();
     }
 
-    public static boolean isWifiConnected(Context context) {
-        NetworkInfo activeNetworkInfo = getActiveNetworkInfo(context);
-        if (activeNetworkInfo != null && activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-            return true;
+    /**
+     * Get network type name
+     * 
+     * @param context
+     * @return
+     */
+    public static String getNetworkTypeName(Context context) {
+        ConnectivityManager manager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo;
+        String type = NETWORK_TYPE_DISCONNECT;
+        if (manager == null || (networkInfo = manager.getActiveNetworkInfo()) == null) {
+            return type;
+        };
+
+        if (networkInfo.isConnected()) {
+            String typeName = networkInfo.getTypeName();
+            if ("WIFI".equalsIgnoreCase(typeName)) {
+                type = NETWORK_TYPE_WIFI;
+            } else if ("MOBILE".equalsIgnoreCase(typeName)) {
+                String proxyHost = android.net.Proxy.getDefaultHost();
+                type = TextUtils.isEmpty(proxyHost) ? (isFastMobileNetwork(context) ? NETWORK_TYPE_3G : NETWORK_TYPE_2G)
+                        : NETWORK_TYPE_WAP;
+            } else {
+                type = NETWORK_TYPE_UNKNOWN;
+            }
         }
-        return false;
+        return type;
     }
 
-    private static NetworkInfo getActiveNetworkInfo(Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        return cm.getActiveNetworkInfo();
+    /**
+     * Whether is fast mobile network
+     * 
+     * @param context
+     * @return
+     */
+    private static boolean isFastMobileNetwork(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager == null) {
+            return false;
+        }
+
+        switch (telephonyManager.getNetworkType()) {
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EHRPD:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_HSPAP:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_IDEN:
+                return false;
+            case TelephonyManager.NETWORK_TYPE_LTE:
+                return true;
+            case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                return false;
+            default:
+                return false;
+        }
     }
 }
